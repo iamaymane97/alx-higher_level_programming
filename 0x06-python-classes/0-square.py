1730d3a89593195dd5a61abf48eefab56eee9b36
iamaymane97 <aymaneelallouch@gmail.com>
2024-01-23 07:29:54 -0800

    all

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -5,11 +5,4 @@
 
 class Square:
     """Represent a square."""
-    pas#!/usr/bin/python3
-
-    """Define a class Square."""
-
-
-    class Square:
-            """Represent a square."""
-                passs
+    pass
